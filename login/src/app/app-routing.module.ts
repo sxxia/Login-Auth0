f4ef588cfee237ffd6818502995a31a79ff89869
Shuang Xia <shuangx@stradigi.ai>
2019-07-30 15:50:52 -0400

    add callback component

--- login/src/app/app-routing.module.ts
+++ login/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CallbackComponent } from './callback/callback.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'callback',
+    component: CallbackComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
